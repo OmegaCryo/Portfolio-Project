d7f0606658b55835fcadaa9bda3a092bff7efc2d
Ashley Chatman <ashchat2008@gmail.com>
2026-05-27 21:15:39 -0400

    Javascript -Add script files -Add clock id

---
+++ script.js
@@ -0,0 +1,12 @@
+//Digital Clock
+function currentTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, 0);
+  const minutes = now.getMinutes().toString().padStart(2, 0);
+  const seconds = now.getSeconds().toString().padStart(2, 0);
+
+  const timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("clock").textContent = timeString;
+}
+currentTime();
+setInterval(currentTime, 1000);
